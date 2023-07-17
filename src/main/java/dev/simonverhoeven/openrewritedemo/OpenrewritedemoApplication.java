a8e5d4e82b21e138d20ff86837fd058c3ad0c398
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-07-17 19:11:10 +0200

    feat: initial commit
    Signed-off-by: Simon Verhoeven <verhoeven.simon@gmail.com>

---
+++ src/main/java/dev/simonverhoeven/openrewritedemo/OpenrewritedemoApplication.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.openrewritedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrewritedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenrewritedemoApplication.class, args);
+	}
+
+}
