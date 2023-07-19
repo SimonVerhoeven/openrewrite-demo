e612fbffb5438bcc9132705d4fcf80329e0d3292
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-07-19 18:21:49 +0200

    feat: documentation of a basic recipe without parameters

--- src/main/java/dev/simonverhoeven/openrewritedemo/OpenrewritedemoApplication.java
+++ src/main/java/dev/simonverhoeven/openrewritedemo/OpenrewritedemoApplication.java
@@ -3,10 +3,12 @@ package dev.simonverhoeven.openrewritedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class OpenrewritedemoApplication {
 
-	public static void main(String[] args) {;
+	public static void main(String[] args) {
 		SpringApplication.run(OpenrewritedemoApplication.class, args);
 	}
 
