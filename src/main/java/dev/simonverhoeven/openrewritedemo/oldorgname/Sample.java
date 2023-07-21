3619f25e760d4ddcea678058342fb1a1c07bc758
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-07-21 15:06:50 +0200

    feat: add more outdated core to demonstrate migration options

--- src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/Sample.java
+++ src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/Sample.java
@@ -1,4 +1,23 @@
 package dev.simonverhoeven.openrewritedemo.oldorgname;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class Sample {
+
+    public void sampleFunction() {
+        Boolean sample = true;
+        BigDecimal versionCost = BigDecimal.valueOf(8);
+        versionCost.setScale(2, 1);
+
+        Optional<String> emptyOptional = Optional.empty();
+        boolean isOptionalEmpty = !emptyOptional.isPresent();
+
+        String text = String.format("This is really %s", "outdated");
+
+        String paragraph = "It is really annoying\n" +
+                "that we have to\n" +
+                "keep adding\n" +
+                "these lin breaks\n";
+    }
 }
