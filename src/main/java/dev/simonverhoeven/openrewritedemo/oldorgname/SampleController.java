5f6b70607732758f7b76cf37f3982976d6bb6a16
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-07-25 17:44:40 +0200

    feat: use older fashion of mapping a getter

--- src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/SampleController.java
+++ src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/SampleController.java
@@ -1,15 +1,16 @@
 package dev.simonverhoeven.openrewritedemo.oldorgname;
 
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
     Logger logger = Logger.getLogger(SampleController.class);
 
-    @GetMapping("/greet/{name}")
+    @RequestMapping(method = RequestMethod.GET, value = "/greet/{name}")
     public String greet(@PathVariable("name") String name) {
         logger.info("We're responding to " + name);
         return "Hello " + name;
