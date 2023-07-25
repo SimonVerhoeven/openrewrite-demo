4b53e49ed5bd5c4a5396deb5886d2ca4672707ac
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-07-25 16:37:48 +0200

    feat: document log4j -> log4j2 migration

--- src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/SampleController.java
+++ src/main/java/dev/simonverhoeven/openrewritedemo/oldorgname/SampleController.java
@@ -1,14 +1,17 @@
 package dev.simonverhoeven.openrewritedemo.oldorgname;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    Logger logger = Logger.getLogger(SampleController.class);
 
     @GetMapping("/greet/{name}")
     public String greet(@PathVariable("name") String name) {
+        logger.info("We're responding to " + name);
         return "Hello " + name;
     }
 }
