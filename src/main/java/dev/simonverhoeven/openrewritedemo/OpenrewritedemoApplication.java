bb1eea1d6d99f71c510a8961ab5792c1ca231f3e
Simon Verhoeven <simon.verhoeven@aquafin.be>
2023-10-03 10:17:47 +0200

    fix: readd unused import for demo

--- src/main/java/dev/simonverhoeven/openrewritedemo/OpenrewritedemoApplication.java
+++ src/main/java/dev/simonverhoeven/openrewritedemo/OpenrewritedemoApplication.java
@@ -3,6 +3,8 @@ package dev.simonverhoeven.openrewritedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class OpenrewritedemoApplication {
 
